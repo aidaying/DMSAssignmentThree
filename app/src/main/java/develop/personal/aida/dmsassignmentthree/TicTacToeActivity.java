066374fee78b86c183962a721ffa923dcb20144a
Aida <aida_ying@hotmail.com>
2016-05-11 16:52:36 +1200

    Initial Commit

---
+++ app/src/main/java/develop/personal/aida/dmsassignmentthree/TicTacToeActivity.java
@@ -0,0 +1,27 @@
+package develop.personal.aida.dmsassignmentthree;
+
+import android.bluetooth.BluetoothAdapter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class BluetoothActivity extends AppCompatActivity {
+
+    private TextView statusTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tic_tac_toe);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if(bluetoothAdapter == null){
+            statusTextView.setText(R.string.status_unsupported);
+
+        }
+    }
+}
