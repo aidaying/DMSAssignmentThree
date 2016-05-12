f03d1060518dcc54f40c2c5209f3d4917feca65b
Aida <aida_ying@hotmail.com>
2016-05-12 15:00:18 +1200

    update nfs

--- app/src/main/java/develop/personal/aida/dmsassignmentthree/nfc.java
+++ app/src/main/java/develop/personal/aida/dmsassignmentthree/nfc.java
@@ -1,27 +1,13 @@
 package develop.personal.aida.dmsassignmentthree;
 
-import android.bluetooth.BluetoothAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
 
-public class BluetoothActivity extends AppCompatActivity {
-
-    private TextView statusTextView;
+public class nfc extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nfc);
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if(bluetoothAdapter == null){
-            statusTextView.setText(R.string.status_unsupported);
-
-        }
     }
 }
