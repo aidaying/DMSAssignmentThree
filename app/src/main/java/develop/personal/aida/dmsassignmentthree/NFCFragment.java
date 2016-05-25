56625b0edc02eb994ea808491ad8c5c88887e208
Aida <aida_ying@hotmail.com>
2016-05-25 18:38:50 +1200

    second commit
    removed tic tac toe added NFC didnt do much coding tho

---
+++ app/src/main/java/develop/personal/aida/dmsassignmentthree/NFCFragment.java
@@ -0,0 +1,29 @@
+package develop.personal.aida.dmsassignmentthree;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class NFCFragment extends Fragment {
+
+
+    public NFCFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_nfc, container, false);
+    }
+
+}
