4d34d7983aba97acbc075643ecf598b75deb0d92
hbp0048 <minjupk@hotmail.com>
2016-05-29 20:23:33 +1200

    messaging

--- app/src/main/java/develop/personal/aida/dmsassignmentthree/SendSMS.java
+++ app/src/main/java/develop/personal/aida/dmsassignmentthree/SendSMS.java
@@ -1,6 +1,7 @@
 package develop.personal.aida.dmsassignmentthree;
 
 import android.app.Activity;
+import android.app.FragmentManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.IntentFilter;
@@ -8,6 +9,7 @@ import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 /**
@@ -16,6 +18,8 @@ import android.widget.TextView;
 public class SendSMS extends Activity implements View.OnClickListener {
 
 
+    public static final String SELECTED_PHONE = "selectedphone";
+    public static final int SUCCESS = 1;
     private Button sendButton;
     private Button contactButton;
     private Button nfcButton;
@@ -23,17 +27,33 @@ public class SendSMS extends Activity implements View.OnClickListener {
     private MessageDeliveredBroadcastReceiver deliveredBroadcastReceiver;
     private final String SMS_SENT_ACTION = "SMS_SENT";
     private final String SMS_DELIVERED_ACTION = "SMS_DELIVERED";
+    private EditText pnumber;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sms);
         sendButton = (Button) findViewById(R.id.send_button);
         sendButton.setOnClickListener(this);
         contactButton = (Button) findViewById(R.id.contact_button);
         contactButton.setOnClickListener(this);
         nfcButton = (Button) findViewById(R.id.nfc_button);
         nfcButton.setOnClickListener(this);
+
+        contactButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                ContactsDialog contactsDialog = new ContactsDialog();
+                FragmentManager fm = getFragmentManager();
+                contactsDialog.show(fm, "");
+                contactsDialog.setDialogResult(new ContactsDialog.OnMyDialogResult(){
+                    public void finish(String result){
+                        pnumber = (EditText) findViewById(R.id.number_text);
+                        pnumber.setText(result);
+                    }
+                });
+
+            }
+        });
     }
 
     @Override
@@ -80,11 +100,23 @@ public class SendSMS extends Activity implements View.OnClickListener {
             smsManager.sendTextMessage(numberString, null, messageString, sentPendingIntent, deliveredPendingIntent);
         }
 
-        if(view == contactButton) {
 
 
-        }
 
     }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 10001) {
+            if (resultCode == SUCCESS) {
+                ((TextView)findViewById(R.id.number_text)).setText(data.getStringExtra(SELECTED_PHONE));
+            } else {
+                ((TextView)findViewById(R.id.number_text)).setText("");
+            }
+        }
+    }
+
 
 }
